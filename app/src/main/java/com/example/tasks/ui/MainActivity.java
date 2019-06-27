93bbbceeeb26e844c965afe9019f3e3ec98d9ded
Bogdan <mv-bogdan@yandex.com>
2019-06-27 19:29:36 +0300

    270619

--- app/src/main/java/com/example/tasks/ui/MainActivity.java
+++ app/src/main/java/com/example/tasks/ui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tasks.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
 
-import com.example.tasks.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.tasks.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
